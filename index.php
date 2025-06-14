b5289dc5e0cb6be6238ac6b8251879c74cb2c723
opamao11 <oluseunamao1@gmail.com>
2025-06-13 21:35:22 -0500

    Initial commit: Add PHP app for Azure deployment

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BallotOnline POC</title>
+</head>
+<body>
+    <h1>Congratulations!</h1>
+    <p>This PHP page is hosted on AWS using Elastic Beanstalk.</p>
+</body>
+</html>

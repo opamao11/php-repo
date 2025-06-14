2ed951925dff399090439ca37212b794acb1d684
opamao11 <oluseunamao1@gmail.com>
2025-06-13 22:13:14 -0500

    Initial commit: Add PHP app for Azure deploymentt

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
-<!DOCTYPE html>
-<html>
+<?
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $file = '/tmp/sample-app.log';
+  $message = file_get_contents('php://input');
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
+}
+else
+{
+?>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>BallotOnline POC</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>PHP Application - BallotOnline POC</title>
+    <meta name="viewport" content="width=device-width">
+
 </head>
 <body>
-    <h1>Congratulations!</h1>
-    <p>This PHP page is hosted on AWS using Elastic Beanstalk.</p>
+    <section class="congratulations">
+        <h1>Congratulations!</h1>
+        <p>Your BallotOnline POC PHP Web application is now running in your own dedicated environment in Microsoft Azure App Service!</p>
+        <p>You are running PHP version <?= phpversion() ?></p>
+    </section>
 </body>
 </html>
+<? 
+} 
+?>
+
+
+
